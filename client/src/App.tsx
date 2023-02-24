c144ebef3594c7ebec153f01dc2fd63eeda03199
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-02-24 02:05:43 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -62,7 +62,7 @@ function App() {
 
       // Is a user is logged in?
       if (profileObj) {
-        const response = await fetch('http://localhost:8080/api/v1/users', {
+        const response = await fetch('https://real-estate-admin-dashboard.onrender.com/api/v1/users', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
@@ -131,7 +131,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://real-estate-admin-dashboard.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
